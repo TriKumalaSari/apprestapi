3ef3510267750aed9c51b59b6f215ecaa95519b2
Tri Kumala Sari <sarii63782@gmail.com>
2023-11-26 21:34:40 +0700

    menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi Rest Api Ku Berjalan!") 
+    response.ok("Aplikasi Rest Api Ku Berjalan!",res) 
+};
+
+//Menampilkan semua data mahasiswa
+exports.tampilsemuadatamahasiswa = function(req,res){
+   connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+    if(eror){
+        connection.log(error);
+    }else {
+        response.ok(rows, res)
+    }
+   });
 };
